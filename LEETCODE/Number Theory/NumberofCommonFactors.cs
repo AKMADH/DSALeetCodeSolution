6ef1fed975968edbce72ac377c9568a72848ddb6
Anish kumar mishra <anishkumarmishra@Anishs-Laptop.local>
2025-02-09 16:24:29 +0400

    Common Factor

---
+++ LEETCODE/Number Theory/NumberofCommonFactors.cs
@@ -0,0 +1,41 @@
+﻿public class NumberofCommonFactors
+{
+
+    public int CommonFactors(int a, int b)
+    {
+        int gno = Gcd(a, b);
+        int res = CountDivision(gno);
+        return res;
+    }
+
+   public int  Gcd(int a,int b)
+    {
+        while(b!=0){
+            int temp = b;
+            b=a%b;
+            a=temp;
+
+        }
+        return a;
+
+    }
+    public int CountDivision(int n)
+    {
+        int count = 0;
+        for (int i = 1; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0 && n/i!=i)
+            {
+                count+=2;
+            }
+            else if (n % i == 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
+
+
