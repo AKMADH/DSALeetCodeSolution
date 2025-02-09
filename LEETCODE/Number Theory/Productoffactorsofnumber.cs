6107385ec131ad8d0058266994346cfd918d3455
Anish kumar mishra <anishkumarmishra@Anishs-Laptop.local>
2025-02-09 16:54:34 +0400

    product of factors

---
+++ LEETCODE/Number Theory/Productoffactorsofnumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Application
+{
+	public class Productoffactorsofnumber
+	{
+		public int ProductOfFactor(int n)
+        {
+             long product = 1;
+            long mod = 100000007;
+            for (int i = 1; i*i <=n ; i++)
+            {
+                if (n % i == 0)
+                {
+                    if (n / i == i)
+                    {
+                        product =(product*i)%mod;
+                    }
+                    else
+                    {
+                        product = (product * i * (n / i)) % mod;
+                    }
+                }
+            }
+            return product;
+        }
+	}
+}
+
