a28244bde00455a7ff10c30d2c692a50ee5d5c5f
Anish kumar mishra <anishkumarmishra@Anishs-Laptop.local>
2025-02-10 21:30:48 +0400

    intersect array

---
+++ LEETCODE/Searching/IntersectTwoArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Application
+{
+	public class IntersectTwoArray
+	{
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            Dictionary<int, int> freq = new Dictionary<int, int>();
+            List<int> result = new List<int>();
+
+            foreach (int num in nums1)
+            {
+                if (freq.ContainsKey(num))
+                {
+                    freq[num]++;
+                }
+                else
+                {
+                    freq.Add(num, 1);
+                }
+            }
+
+            foreach (int num in nums2)
+            {
+                if (freq.ContainsKey(num) && freq[num] > 0)
+                {
+                    result.Add(num);
+                    freq[num]--;
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
+
