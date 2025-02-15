d6223751d3bc9a1d8649989c733d59238212ab39
Anish kumar mishra <anishkumarmishra@Anishs-Laptop.local>
2025-02-15 19:25:46 +0400

    xor operation subset array sum

---
+++ LEETCODE/Bit Manupulation/SubsetXorSum.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int SubsetXORSum(int[] nums)
+    {
+        return SumXor(nums, 0, 0);
+    }
+
+    public int SumXor(int[] nums, int i, int current)
+    {
+        if (nums.Length == i)
+        {
+            return current;
+        }
+        return SumXor(nums, i + 1, current ^ nums[i]) + SumXor(nums, i + 1, current);
+    }
+}
